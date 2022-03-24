51581d13de13baa7d91b262c8b76896e24a8efd5
Martin Avramov <martinddesigns@yahoo.com>
2022-03-24 23:38:48 +0200

    Deleting no need files and setting up Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './styles/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-[32px] font-bold">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
