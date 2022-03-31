ca526cf05d9073b9298f2f675eebd0b94a1ec386
devmarto <martinddesigns@yahoo.com>
2022-03-31 17:05:04 +0300

    added styling

--- src/Adapters/MovieList.jsx
+++ src/Adapters/MovieList.jsx
@@ -4,30 +4,33 @@ import Movie from './Movie';
 
 export default function MovieList({movieSearch, loader}) {
 
-
-  
-    const filteredArray = Movies.filter((movie) => {
-      if (movieSearch == '') {
-        return movie;
-      } else if (movie.title.toLowerCase().includes(movieSearch.toLowerCase())) {
-        return movie;
-      }
-    })  
+  const filteredArray = Movies.filter((movie) => {
+    if (movieSearch == '') {
+      return movie;
+    } else if (movie.title.toLowerCase().includes(movieSearch.toLowerCase())) {
+      return movie;
+    }
+  })  
 
   return (
-      <section className="mvlist">
-        <div className="container">
-          {loader ?
-          <h1 className='text-center'>Loading...</h1> :
+    <section className="mvlist">
+      <div className="container">
+        {loader ? <span className='bg-indigo-500 py-[10px] px-[20px] text-center mx-auto inline-block rounded text-white font-bold'>Loading</span> :
+
+          filteredArray.length !== 0 ? 
+          
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 justify-center gap-[20px]">
-            {filteredArray.length !== 0 ? filteredArray.map((movie, id) => (
-              <Link key={id} to={`/movie/${movie.id}`}>
-                <Movie title={movie.title} image={movie.imageUrl} rank={movie.rank} />
-              </Link>
-            )) : <p className='text-center'>Nothing was found</p>}
+      
+          {filteredArray.map((movie, id) => (
+            <Link key={id} to={`/movie/${movie.id}`}>
+              <Movie title={movie.title} image={movie.imageUrl} rank={movie.rank} />
+            </Link>
+          ))}
             <Outlet />
           </div>
-          }
+
+          : <span className='bg-indigo-500 py-[10px] px-[20px] text-center mx-auto inline-block rounded text-white font-bold'>Nothing was found</span>
+        }
       </div>
     </section>
   )
--- src/movies.js
+++ src/movies.js
@@ -62,451 +62,541 @@ export const Movies = [
   {
     "title": "Star Wars: Episode V - The Empire Strikes Back",
     "rank": "11",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0080684"
   },
   {
     "title": "The Lord of the Rings: The Fellowship of the Ring",
     "rank": "12",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0120737"
   },
   {
     "title": "One Flew Over the Cuckoo's Nest",
     "rank": "13",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0073486"
   },
   {
     "title": "Inception",
     "rank": "14",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt1375666"
   },
   {
     "title": "Goodfellas",
     "rank": "15",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0099685"
   },
   {
     "title": "Star Wars",
     "rank": "16",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0076759"
   },
   {
     "title": "Seven Samurai",
     "rank": "17",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0047478"
   },
   {
     "title": "Forrest Gump",
     "rank": "18",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0109830"
   },
   {
     "title": "The Matrix",
     "rank": "19",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0133093"
   },
   {
     "title": "The Lord of the Rings: The Two Towers",
     "rank": "20",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0167261"
   },
   {
     "title": "City of God",
     "rank": "21",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0317248"
   },
   {
     "title": "Se7en",
     "rank": "22",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0114369"
   },
   {
     "title": "The Silence of the Lambs",
     "rank": "23",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0102926"
   },
   {
     "title": "Once Upon a Time in the West",
     "rank": "24",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0064116"
   },
   {
     "title": "Casablanca",
     "rank": "25",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0034583"
   },
   {
     "title": "The Usual Suspects",
     "rank": "26",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0114814"
   },
   {
     "title": "Raiders of the Lost Ark",
     "rank": "27",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0082971"
   },
   {
     "title": "Rear Window",
     "rank": "28",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0047396"
   },
   {
     "title": "It's a Wonderful Life",
     "rank": "29",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0038650"
   },
   {
     "title": "Psycho",
     "rank": "30",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0054215"
   },
   {
     "title": "Léon: The Professional",
     "rank": "31",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0110413"
   },
   {
     "title": "Sunset Blvd.",
     "rank": "32",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0043014"
   },
   {
     "title": "American History X",
     "rank": "33",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0120586"
   },
   {
     "title": "Apocalypse Now",
     "rank": "34",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0078788"
   },
   {
     "title": "Terminator 2: Judgment Day",
     "rank": "35",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0103064"
   },
   {
     "title": "Saving Private Ryan",
     "rank": "36",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0120815"
   },
   {
     "title": "Memento",
     "rank": "37",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0209144"
   },
   {
     "title": "City Lights",
     "rank": "38",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0021749"
   },
   {
     "title": "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
     "rank": "39",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0057012"
   },
   {
     "title": "Alien",
     "rank": "40",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0078748"
   },
   {
     "title": "Modern Times",
     "rank": "41",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0027977"
   },
   {
     "title": "Spirited Away",
     "rank": "42",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0245429"
   },
   {
     "title": "North by Northwest",
     "rank": "43",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0053125"
   },
   {
     "title": "Back to the Future",
     "rank": "44",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0088763"
   },
   {
     "title": "Life Is Beautiful",
     "rank": "45",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0118799"
   },
   {
     "title": "The Shining",
     "rank": "46",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0081505"
   },
   {
     "title": "The Pianist",
     "rank": "47",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0253474"
   },
   {
     "title": "Citizen Kane",
     "rank": "48",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0033467"
   },
   {
     "title": "The Departed",
     "rank": "49",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0407887"
   },
   {
     "title": "M",
     "rank": "50",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0022100"
   },
   {
     "title": "Paths of Glory",
     "rank": "51",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0050825"
   },
   {
     "title": "Vertigo",
     "rank": "52",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0052357"
   },
   {
     "title": "Django Unchained",
     "rank": "53",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt1853728"
   },
   {
     "title": "Double Indemnity",
     "rank": "54",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0036775"
   },
   {
     "title": "The Dark Knight Rises",
     "rank": "55",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt1345836"
   },
   {
     "title": "Aliens",
     "rank": "56",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0090605"
   },
   {
     "title": "Taxi Driver",
     "rank": "57",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0075314"
   },
   {
     "title": "American Beauty",
     "rank": "58",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0169547"
   },
   {
     "title": "The Green Mile",
     "rank": "59",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0120689"
   },
   {
     "title": "Gladiator",
     "rank": "60",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0172495"
   },
   {
     "title": "The Intouchables",
     "rank": "61",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt1675434"
   },
   {
     "title": "WALL·E",
     "rank": "62",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0910970"
   },
   {
     "title": "The Lives of Others",
     "rank": "63",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0405094"
   },
   {
     "title": "Toy Story 3",
     "rank": "64",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0435761"
   },
   {
     "title": "The Great Dictator",
     "rank": "65",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0032553"
   },
   {
     "title": "The Prestige",
     "rank": "66",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0482571"
   },
   {
     "title": "A Clockwork Orange",
     "rank": "67",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0066921"
   },
   {
     "title": "Lawrence of Arabia",
     "rank": "68",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0056172"
   },
   {
     "title": "Amélie",
     "rank": "69",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0211915"
   },
   {
     "title": "To Kill a Mockingbird",
     "rank": "70",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0056592"
   },
   {
     "title": "Reservoir Dogs",
     "rank": "71",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0105236"
   },
   {
     "title": "Das Boot",
     "rank": "72",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0082096"
   },
   {
     "title": "The Lion King",
     "rank": "73",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0110357"
   },
   {
     "title": "Cinema Paradiso",
     "rank": "74",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0095765"
   },
   {
     "title": "Star Wars: Episode VI - Return of the Jedi",
     "rank": "75",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0086190"
   },
   {
     "title": "The Treasure of the Sierra Madre",
     "rank": "76",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0040897"
   },
   {
     "title": "The Third Man",
     "rank": "77",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0041959"
   },
   {
     "title": "Once Upon a Time in America",
     "rank": "78",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0087843"
   },
   {
     "title": "Requiem for a Dream",
     "rank": "79",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0180093"
   },
   {
     "title": "Eternal Sunshine of the Spotless Mind",
     "rank": "80",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0338013"
   },
   {
     "title": "Full Metal Jacket",
     "rank": "81",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0093058"
   },
   {
     "title": "Oldboy",
     "rank": "82",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0364569"
   },
   {
     "title": "Braveheart",
     "rank": "83",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0112573"
   },
   {
     "title": "L.A. Confidential",
     "rank": "84",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0119488"
   },
   {
     "title": "Bicycle Thieves",
     "rank": "85",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0040522"
   },
   {
     "title": "Chinatown",
     "rank": "86",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0071315"
   },
   {
     "title": "Singin' in the Rain",
     "rank": "87",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0045152"
   },
   {
     "title": "Princess Mononoke",
     "rank": "88",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0119698"
   },
   {
     "title": "Monty Python and the Holy Grail",
     "rank": "89",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0071853"
   },
   {
     "title": "Metropolis",
     "rank": "90",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0017136"
   },
   {
     "title": "Rashomon",
     "rank": "91",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0042876"
   },
   {
     "title": "Some Like It Hot",
     "rank": "92",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0053291"
   },
   {
     "title": "Amadeus",
     "rank": "93",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0086879"
   },
   {
     "title": "2001: A Space Odyssey",
     "rank": "94",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0062622"
   },
   {
     "title": "All About Eve",
     "rank": "95",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0042192"
   },
   {
     "title": "Witness for the Prosecution",
     "rank": "96",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0051201"
   },
   {
     "title": "The Sting",
     "rank": "97",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0070735"
   },
   {
     "title": "The Apartment",
     "rank": "98",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0053604"
   },
   {
     "title": "Grave of the Fireflies",
     "rank": "99",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0095327"
   },
   {
     "title": "Indiana Jones and the Last Crusade",
     "rank": "100",
+    "imageUrl": "https://m.media-amazon.com/images/I/51zUbui+gbL._SY445_.jpg",
     "id": "tt0097576"
   }
 ]
